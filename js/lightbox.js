8d3ab869800df5c7c2d5a8aec7532bc3c989f173
MeesPos <meespostma_@hotmail.com>
2020-02-24 14:43:48 +0100

    Laatste gedeelte

--- js/lightbox.js
+++ js/lightbox.js
@@ -10,19 +10,32 @@ for(let i=0; i<watWegMoet.length; i++) {
     watWegMoet [i].remove(); 
 } 
 
+const maakSluitKnop = document.createElement('i');
+maakSluitKnop.className = 'fas fa-times-circle sk';
+maakSluitKnop.addEventListener('click', verwijderModaal)
+
 function maakModaal(num) {
     console.log('geklikt op '+num)
     console.log(alleInfo[num].innerHTML)
     let modaal = document.createElement('div');
     modaal.id= 'modaal';
+    modaal.addEventListener('click', verwijderModaal);
     let modaalInhoud = document.createElement('div');
     modaalInhoud.className = 'modaal-inhoud';
     modaalInhoud.innerHTML = alleInfo[num].innerHTML;
+    modaalInhoud.addEventListener("click", function(e) {
+        e.stopPropagation();
+    })
+    modaalInhoud.prepend(maakSluitKnop);
     modaal.append(modaalInhoud);
     document.body.append(modaal);
 }
 
 // Klik gebeurtenis op de kleine div's
+
+function verwijderModaal() {
+    document.getElementById("modaal").remove();
+}
 
 for(let i=0; i<deKleintjes.length; i++) {
     deKleintjes[i].addEventListener('click', function() {
