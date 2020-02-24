0b6d1d880d1f9c521ee5ac6a78ee90dd6202a458
MeesPos <meespostma_@hotmail.com>
2020-02-24 11:47:41 +0100

    Tweede uur

--- js/lightbox.js
+++ js/lightbox.js
@@ -7,5 +7,25 @@ const alleInfo = []
 for(let i=0; i<watWegMoet.length; i++) {
     alleInfo.push(watWegMoet [i]);
     // element uit de DOM verwijderen
-    watWegMoet [i].remove();
+    watWegMoet [i].remove(); 
 } 
+
+function maakModaal(num) {
+    console.log('geklikt op '+num)
+    console.log(alleInfo[num].innerHTML)
+    let modaal = document.createElement('div');
+    modaal.id= 'modaal';
+    let modaalInhoud = document.createElement('div');
+    modaalInhoud.className = 'modaal-inhoud';
+    modaalInhoud.innerHTML = alleInfo[num].innerHTML;
+    modaal.append(modaalInhoud);
+    document.body.append(modaal);
+}
+
+// Klik gebeurtenis op de kleine div's
+
+for(let i=0; i<deKleintjes.length; i++) {
+    deKleintjes[i].addEventListener('click', function() {
+        maakModaal(i)
+    });
+}
